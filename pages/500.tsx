be42198f2e54ce3fff0f3c25a0010d5d8fff3b58
Samuel Porto <samuel.porto@bizsys.com.br>
2021-11-22 18:00:00 -0300

    pagina de erro 500

---
+++ pages/500.tsx
@@ -0,0 +1,20 @@
+import styles from "./404.module.scss";
+import notFound from "../assets/svg/not_found.svg";
+import Image from "next/image" 
+import Link from "next/link" 
+
+const Custom500 = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Sorry, this page not exist or we have a internal server error. <Link href="/">Go back to home.</Link></h1>
+      <Image
+        src={notFound}
+        height={500}
+        width={500}
+        alt="Not Found"
+      />
+    </div>
+  );
+};
+
+export default Custom500;
